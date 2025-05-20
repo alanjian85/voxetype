428bd353b62ef9611dba190bae5235a44f07f882
Alan Jian <alanjian85@outlook.com>
2025-05-20 23:49:47 +0800

    feat: add event handling for moving camera

--- src/lib.rs
+++ src/lib.rs
@@ -27,21 +27,22 @@ pub fn run(width: usize, height: usize) -> Result<(), Box<dyn Error>> {
     let mut stdin = termion::async_stdin();
     write!(stdout, "{}", cursor::Hide)?;
 
-    let view_mat = DMat4::look_to_rh(
-        DVec3::new(0.0, 0.0, 5.0),
-        DVec3::new(0.0, 0.0, -1.0),
-        DVec3::new(0.0, 1.0, 0.0),
-    );
     let aspect_ratio = (width * FULL_BLOCK_WIDTH) as f64 / (height * FULL_BLOCK_HEIGHT) as f64;
     let proj_mat = DMat4::perspective_rh(45.0f64.to_radians(), aspect_ratio, 0.01, 1000.0);
 
     let framebuffer = Framebuffer::new(width, height, color::Rgb(98, 9, 92));
     let mut renderer = Renderer::new(framebuffer);
+    let mut cam_pos = DVec3::new(0.0, 0.0, 5.0);
     let start_time = Instant::now();
     'game_loop: loop {
         for c in stdin.by_ref().bytes() {
-            if c? == b'q' {
-                break 'game_loop Ok(());
+            match c? {
+                b'q' => break 'game_loop Ok(()),
+                b'w' => cam_pos.z -= 0.1,
+                b'a' => cam_pos.x -= 0.1,
+                b's' => cam_pos.z += 0.1,
+                b'd' => cam_pos.x += 0.1,
+                _ => (),
             }
         }
 
@@ -49,6 +50,11 @@ pub fn run(width: usize, height: usize) -> Result<(), Box<dyn Error>> {
 
         let time = start_time.elapsed().as_secs_f64();
         let model_mat = DMat4::from_axis_angle(DVec3::new(1.0, 1.0, 1.0).normalize(), time);
+        let view_mat = DMat4::look_to_rh(
+            cam_pos,
+            DVec3::new(0.0, 0.0, -1.0),
+            DVec3::new(0.0, 1.0, 0.0),
+        );
         renderer.set_transform_mat(proj_mat * view_mat * model_mat);
 
         let normals = [
